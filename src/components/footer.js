e4b8c96449928f8421112949d09cec38eb91eedc
wojcikewelina <gargula.ewe@gmail.com>
2020-09-23 10:09:22 +0200

    2 files modified, 1 added - footer modification

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from "react";
 
-export default function Footer () {
-  return(
+import "../style/footer.scss"
+import Logo from "./header/logo"
+
+export default function Footer() {
+  return (
     <footer>
-    <div id="logo-el"></div>
-    <article id="content">
-    <div className="info-element">
-    <ul>About us
-    <li>Regulamin</li>
-    <li></li>
-    <li></li>
-    </ul>
-    </div>
-    <div className="info-element">
-      <ul>Zobacz wiecej
-    <li><a href="https://www.facebook.com">Facebook</a></li>
-    <li><a href="https://www.twitter.com">Twitter</a></li>
-    <li><a href="https://www.instagram.com">Instagram</a></li>
-    </ul>
-    </div>
-    </article>
+      <div id="logo-el" ><Logo/></div>
+      <article id="content">
+        <div className="info-element">
+          <ul>
+            About us
+            <li>Regulamin</li>
+            <li>Polityka prywatności</li>
+            <li>Kontakt</li>
+          </ul>
+        </div>
+        <div className="info-element">
+          <ul>
+            Zobacz wiecej
+            <li>
+              <a href="https://www.facebook.com">Facebook</a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com">Twitter</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com">Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </article>
     </footer>
-  )
+  );
 }
